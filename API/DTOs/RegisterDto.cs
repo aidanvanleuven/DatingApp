055e69bdf2c07c1648a3c98d816af3ae80a14978
Aidan VanLeuven <aidanvanleuven@gmail.com>
2021-07-28 16:09:40 -0600

    Remove max password limitation

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,7 +8,7 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }
 
-        [StringLength(8, MinimumLength = 4)]
+        [StringLength(128, MinimumLength = 4)]
         [Required]
         public string Password { get; set; }
 
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -36,7 +36,7 @@ export class RegisterComponent implements OnInit {
       dateOfBirth: ['', Validators.required],
       city: ['', Validators.required],
       country: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(128)]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]]
     });
     this.registerForm.controls.password.valueChanges.subscribe(() => {
